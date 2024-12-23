cf7e115d37943bbf8656ca32659a514664e13369
Khann777 <nurdinov.d777@gmail.com>
2024-12-23 19:59:34 +0600

    updated post.serializers

--- BlogAPI/post/serializers.py
+++ BlogAPI/post/serializers.py
@@ -2,10 +2,28 @@ from rest_framework import serializers
 
 from .models import Post
 from category.models import Category
+from comments.serializers import CommentSerializer
 
 class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     category = serializers.PrimaryKeyRelatedField(required=True, queryset=Category.objects.all())
+
+    comments = serializers.SerializerMethodField()
+    likes = serializers.SerializerMethodField()
+    favorites = serializers.SerializerMethodField()
+
+    def get_comments(self, instance):
+        comments = instance.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return serializer.data
+
+    def get_likes_counter(self, instance):
+        likes = instance.likes.all().count()
+        return likes
+
+    def get_favorites_counter(self, instance):
+        favorites = instance.favorites.all().count()
+        return favorites
 
     class Meta:
         model = Post
